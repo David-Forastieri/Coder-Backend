7747f22f4397aa3c0af8693cec6449d6bf5af87d
David-Forastieri <davidforastieri@gmail.com>
2023-07-27 20:46:43 -0300

    Desafio-05 mas correcciones

--- desafio-05/src/routes/views.router.js
+++ desafio-05/src/routes/views.router.js
@@ -14,4 +14,14 @@ router.get('/realtimeproducts', async (req, res) => {
   res.render('realTimeProducts', { AllProducts })
 })
 
+router.get('/form', async (req, res) => {
+  res.render('form', {})
+})
+
+router.post('/form', async (req, res) => {
+  const { title, description, code, price, stock, category, thumbnails } = req.body
+  await manager.addProduct(title, description, code, price, stock, category, thumbnails)
+  res.redirect('/')
+})
+
 export default router
